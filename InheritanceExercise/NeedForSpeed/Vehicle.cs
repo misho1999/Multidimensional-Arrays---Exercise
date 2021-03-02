e37ce55ff3d7e24839a933104d3cb2fd8b9fe5ec
Mihail Nalbantov <misho_1999@students.softuni.bg>
2021-03-02 18:47:52 +0000

    Added new projects!

---
+++ InheritanceExercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+   public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25 ; 
+        public Vehicle(int horsePower, double fuel)
+        {
+            Fuel = fuel;
+            HorsePower = horsePower;
+        }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public virtual void Drive(double kilometers)
+        {
+            Fuel -= kilometers * FuelConsumption;
+        }
+    }
+}
---
+++ InheritanceExercise/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Elf elf = new Elf("MIsho", 14);
+            System.Console.WriteLine(elf);
+        }
+    }
+}
