5f81a35199d7a737f83c327deab1b1e6eef398e0
Mihail Nalbantov <misho_1999@students.softuni.bg>
2021-03-10 01:16:47 +0000

    Added new projects!

---
+++ C#OOPExercise/Vehicles/Raiding/BaseHero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public abstract class BaseHero
+    {
+        public abstract string Name { get; }
+        public abstract int Power { get;}
+
+        public abstract string CastAbility();
+
+    }
+}
